1be1daad171e9d650339ab6951c07931ac0179d2
Wonsup Yoon <pusnow@kaist.ac.kr>
2025-03-21 16:51:07 +0900

    moving

--- main.cpp
+++ main.cpp
@@ -23,16 +23,11 @@
 
 constexpr size_t write_wait_msec = 400;
 
-struct Pkt {
+struct PktHeader {
   uint8_t head[2];
   uint8_t addr;
   uint8_t cmd;
   uint8_t len;
-  uint8_t modifier;
-  uint8_t padding1;
-  uint8_t code;
-  uint8_t padding2[5];
-  uint8_t sum;
 
   static constexpr uint8_t CMD_KEYBOARD = 0x02;
   static constexpr uint8_t CMD_KEYBOARD_MULTI = 0x03;
@@ -43,6 +38,28 @@ struct Pkt {
     head[0] = 0x57;
     head[1] = 0xab;
   }
+
+  void set_keybd() {
+    addr = 0;
+    cmd = CMD_KEYBOARD;
+    len = 8;
+  }
+
+  void set_mouse() {
+    addr = 0;
+    cmd = CMD_MOUSE_ABS;
+    len = 7;
+  }
+
+} __attribute__((packed));
+
+struct PktKbd {
+  PktHeader head;
+  uint8_t modifier;
+  uint8_t padding1;
+  uint8_t code;
+  uint8_t padding2[5];
+  uint8_t sum;
 
   void set_modifier(bool lctrl, bool lshift, bool lalt, bool lwin, bool rctl,
                     bool rshift, bool ralt, bool rwin) {
@@ -64,12 +81,6 @@ struct Pkt {
     if (rwin)
       modifier |= 0b10000000;
   }
-
-  void set_key_down() {
-    addr = 0;
-    cmd = CMD_KEYBOARD;
-    len = 8;
-  }
   void set_key(uint8_t code) { this->code = code; }
 
   void calc_sum() {
@@ -84,14 +95,49 @@ struct Pkt {
   void make_key_down(uint8_t code, bool lctrl, bool lshift, bool lalt,
                      bool lwin, bool rctl, bool rshift, bool ralt, bool rwin) {
     memset(this, 0, sizeof(*this));
-    set_header();
+    head.set_header();
     set_key(code);
     set_modifier(lctrl, lshift, lalt, lwin, rctl, rshift, ralt, rwin);
-    set_key_down();
+    head.set_keybd();
     calc_sum();
   }
   void make_key_up() {
     make_key_down(0, false, false, false, false, false, false, false, false);
+  }
+
+} __attribute__((packed));
+
+struct PktMouse {
+  PktHeader head;
+  uint8_t fixed_2;
+  uint8_t button;
+  uint16_t x;
+  uint16_t y;
+  uint8_t scroll;
+  uint8_t sum;
+
+  void calc_sum() {
+    size_t sz = sizeof(*this) - 1;
+    uint8_t *ptr = (uint8_t *)this;
+    sum = 0;
+    for (size_t i = 0; i < sz; ++i) {
+      sum += ptr[i];
+    }
+  }
+
+  void make_move(float x_f, float y_f) {
+    memset(this, 0, sizeof(*this));
+    head.set_header();
+    head.set_mouse();
+
+    fixed_2 = 0x2;
+
+    uint16_t x_int = 4096 * x_f;
+    uint16_t y_int = 4096 * y_f;
+    x = x_int;
+    y = y_int;
+
+    calc_sum();
   }
 
 } __attribute__((packed));
@@ -103,9 +149,6 @@ class MainWidget : public QVideoWidget {
 public:
   MainWidget(QString port_name) {
 
-    // setMinimumWidth(640);
-    // setMinimumHeight(480);
-    // resize(640, 480);
     init_perm();
     init_serial(port_name);
   }
@@ -118,11 +161,11 @@ protected:
     uint32_t virtkey = event->nativeVirtualKey();
     uint8_t code = key_to_scancode(virtkey);
     if (code) {
-      pkt.make_key_down(code, ctrl_left, shift_left, alt_left, win_left,
-                        ctrl_right, shift_right, alt_right, win_right);
-      write_pkt();
-      pkt.make_key_up();
-      write_pkt();
+      pkt_kbd.make_key_down(code, ctrl_left, shift_left, alt_left, win_left,
+                            ctrl_right, shift_right, alt_right, win_right);
+      write_pkt_kbd();
+      pkt_kbd.make_key_up();
+      write_pkt_kbd();
     }
     switch (code) {
     case CH9_Control:
@@ -186,11 +229,91 @@ protected:
     QVideoWidget::keyReleaseEvent(event);
   }
 
+  void mousePressEvent(QMouseEvent *event) override {
+    int w_widget = width();
+    int h_widget = height();
+
+    auto size_hint = sizeHint();
+
+    int w_cam = size_hint.width();
+    int h_cam = size_hint.height();
+
+    int h_widget_calc = w_widget * h_cam / w_cam;
+    int w_widget_calc = h_widget * w_cam / h_cam;
+
+    QPointF localPos = event->position();
+
+    int w_press = localPos.x();
+    int h_press = localPos.y();
+
+    float x = 0;
+    float y = 0;
+
+    qDebug() << "w_widget: " << w_widget << " w_cam: " << w_cam
+             << " w_press: " << w_press << " w_widget_calc: " << w_widget_calc
+             << "h_widget: " << h_widget << " h_cam: " << h_cam
+             << " h_press: " << h_press << " h_widget_calc: " << h_widget_calc;
+
+    // w_widget:  640  w_cam:  1920  w_press:  248  w_widget_calc:  853
+    // h_widget:  480  h_cam:  1080  h_press:  189  h_widget_calc:  360
+    if (h_widget > h_widget_calc) {
+      // top/bottom letterbox
+      int letter_size = (h_widget - h_widget_calc) / 2;
+
+      if (h_press < letter_size || h_press > h_widget - letter_size) {
+        QVideoWidget::mousePressEvent(event);
+        return;
+      }
+
+      x = 1.0f * w_press / w_widget;
+      y = 1.0f * (h_press - letter_size) / h_widget_calc;
+
+    } else {
+      // left/right letterbox
+      int letter_size = (w_widget - w_widget_calc) / 2;
+
+      if (w_press < letter_size || w_press > w_widget - letter_size) {
+        QVideoWidget::mousePressEvent(event);
+        return;
+      }
+
+      x = 1.0f * (w_press - letter_size) / w_widget_calc;
+      y = 1.0f * h_press / h_widget;
+    }
+
+    // QPointF localPos = event->position();
+    // auto button = event->button();
+
+    pkt_mouse.make_move(x, y);
+    write_pkt_mouse();
+
+    // qDebug() << "button: " << event->button() << " pos: " << localPos;
+
+    // Pass other mouse events to the base class implementation.
+    QVideoWidget::mousePressEvent(event);
+  }
+
+  // void mouseMoveEvent(QMouseEvent *e) override {
+  //   qDebug("move: %d, %d", e->pos().x(), e->pos().y());
+  //   QVideoWidget::mouseMoveEvent(e);
+  // }
+
 private:
   QSerialPort serial;
-  Pkt pkt;
-  void write_pkt() {
-    serial.write((const char *)&pkt, sizeof(pkt));
+  PktKbd pkt_kbd;
+  PktMouse pkt_mouse;
+
+  void write_pkt_kbd() {
+    serial.write((const char *)&pkt_kbd, sizeof(pkt_kbd));
+    if (serial.waitForBytesWritten(write_wait_msec)) {
+      qDebug() << "Data written successfully.";
+    } else {
+      qDebug() << "Error writing data:" << serial.errorString();
+    }
+  }
+
+  void write_pkt_mouse() {
+    serial.write((const char *)&pkt_mouse, sizeof(pkt_mouse));
     if (serial.waitForBytesWritten(write_wait_msec)) {
       qDebug() << "Data written successfully.";
     } else {
@@ -232,16 +355,7 @@ private:
     auto cam = QMediaDevices::defaultVideoInput();
     m_camera.reset(new QCamera(cam));
     m_captureSession.setCamera(m_camera.data());
-
-    // viewfinder = new QVideoWidget(container);
     m_captureSession.setVideoOutput(this);
-
-    // layout = new QVBoxLayout(container);
-    // layout->setContentsMargins(0, 0, 0, 0); // Remove margins
-    // layout->setSpacing(0);                  // Remove spacing
-    // layout->addWidget(viewfinder);
-
-    // container->show();
 
     m_camera->start();
     qDebug() << "connect cam";
